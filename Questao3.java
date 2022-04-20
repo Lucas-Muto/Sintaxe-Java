58598605b0355569f79e79fcce0682ba1f9fc696
Lucas Muto <81264228+Lucas-Muto@users.noreply.github.com>
2022-04-20 12:46:45 -0300

    Create Questao3.java

---
+++ Questao3.java
@@ -0,0 +1,15 @@
+package Questao3;
+
+public class Questao3 {
+
+    public static void main(String[] args) {
+        int soma=0;
+        int i=1;
+        while(i<1000){
+            soma=soma+i;
+            i++;
+        }
+        System.out.println(soma);
+        
+    }
+}
