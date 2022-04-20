53b1b933a866872c73083d7cf23c176ec6fa7b16
Lucas Muto <81264228+Lucas-Muto@users.noreply.github.com>
2022-04-20 12:53:00 -0300

    Create Questao4.java

---
+++ Questao4.java
@@ -0,0 +1,14 @@
+package Questao4;
+
+public class Questao4 {
+
+    public static void main(String[] args) {
+        int somatorio=1;
+        while(somatorio<=100){
+            if(somatorio%3==0){
+                System.out.println(somatorio);
+            }
+            somatorio++;
+        }
+    }
+}
